2e697de89b305cf3f365311e34c2ea8d31de1dff
dalabonba <upup588@gmail.com>
2023-01-15 06:30:06 +0800

    建立setting資料表的migration

---
+++ database/migrations/2023_01_14_221806_create_setting_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('setting', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('week_days',2)->default('5天');
+            $table->string('number_of_class',1)->default('8');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('setting');
+    }
+}
