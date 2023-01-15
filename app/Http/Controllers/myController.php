6381eb4534a76ccc7c896f0ed4f9ac53ebfee3cb
dalabonba <upup588@gmail.com>
2023-01-16 07:49:50 +0800

    首頁初始畫面顯示完成
    controller的show_index()跟index.blade

--- app/Http/Controllers/myController.php
+++ app/Http/Controllers/myController.php
@@ -3,11 +3,83 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use SebastianBergmann\Type\TrueType;
+use Illuminate\Support\Facades\DB;
 
 class myController extends Controller
 {
     public function show_index()
     {
-        return view('index');
+        //若為初次進入畫面，先建立class資料表資料和setting資料表
+        $db_setting_table = DB::table('setting')->get();
+        if ($db_setting_table->isEmpty()) {
+            DB::table('setting')->insert([
+                'week_days' => 5,
+                'number_of_class' => 8,
+            ]);
+        }
+        $week_days=DB::table('setting')->value('week_days');
+        $number_of_class=DB::table('setting')->value('number_of_class');
+
+        $db_class_table = DB::table('class')->get();
+        if ($db_class_table->isEmpty()) {
+            for ($i = 1; $i <= $week_days*$number_of_class; $i++) {
+                DB::table('class')->insert([
+                    'class_name' => '課程名稱',
+                    'professor' => '教授名字',
+                    'classroom' => '教室位置',
+                ]);
+            }
+            $db_class_table = DB::table('class')->get();//建立資料後再重新定義一次$db_class_table
+        }
+        //----------------
+
+        /*
+        DB::table('class')->truncate();
+        DB::table('setting')->truncate();
+        dd('1');
+        */
+
+        $title='課表';
+
+        return view('index', compact('title','db_class_table','week_days','number_of_class'));
+    }
+
+    public function show_setting()
+    {
+        //若未先進入首頁產生class資料表資料，則重導向至首頁
+        $db_class_table = DB::table('class')->get();
+        if ($db_class_table->isEmpty()) {
+            return redirect(route('show_index'));
+        } else {
+            return view('setting', ['title' => '設定']);
+        }
+    }
+
+    public function store_setting(Request $request)
+    {
+        /*$class_name = $request->get('class_name');
+        $professor = $request->get('professor');
+        $classroom = $request->get('classroom');
+        $homework = $request->get('homework');
+
+        DB::table('todos')->insert([
+            'class_name'=>$class_name,
+            'professor'=>$professor,
+            'classroom'=>$classroom,
+            'homework'=>$homework
+        ]);*/
+
+        $week_days = $request->get('week_days');
+        $number_of_class = $request->get('number_of_class');
+
+        DB::table('setting')
+            ->where('id', $id)
+            ->update([
+                'week_days' => $week_days,
+                'number_of_class' => $number_of_class
+            ]);
+
+        return view('pages.index');
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,23 +1,28 @@
-@extends('bootstrap')
+@extends('layouts.master')
 @section('content')
 
-        <div class="row justify-content-center">
-            <div class="col-2 align-self-center text-center bg-light">
-                <h1 class="display-5">第一節</h1>
-            </div>
-            <div class="col-2">
-                <div class="card MousePointer text-center" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                    <a class="card-body">
-                        <h5 class="card-title">課程名稱</h5>
-                        <ul class="list-group">
-                            <li class="list-group-item">老師名</li>
-                            <li class="list-group-item">教室位置</li>
-                        </ul>
-                    </a>
-                </div>
-            </div>
-
+    @for($i=1;$i<=$number_of_class;$i++){{-- $i:第幾節課、$number_of_class:有幾節課，1節課一個row --}}
+    <div class="row justify-content-center mb-2">
+        <div class="col-2 align-self-center text-center bg-light">{{--每個row開頭插一個節次--}}
+            <h1 class="display-5">第{{$i}}節</h1>
         </div>
 
-        @include('modal')
+        {{-- 假如$week_days=5，代表每5筆class的資料，就要換下一個row --}}
+        @for($j=($i-1)*$week_days+1;$j<=($week_days*$i);$j++)  {{-- 假如$week_days=5，在$i=1時，$j會是1~5，$i=2時，$j是6~10，以此類推  --}}
+        <div @if($week_days==5) class="col-2" @else class="col-1" @endif>
+            <div class="card MousePointer text-center" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                <a class="card-body">
+                    <h5 class="card-title">{{$db_class_table[$j-1]->class_name}}</h5> {{-- 第1筆資料索引值是0所以要減1 --}}
+                    <ul class="list-group">
+                        <li class="list-group-item">{{$db_class_table[$j-1]->professor}}</li>
+                        <li class="list-group-item">{{$db_class_table[$j-1]->classroom}}</li>
+                    </ul>
+                </a>
+            </div>
+        </div>
+        @endfor
+    </div>
+    @endfor
+
+    @include('modal')
 @endsection
